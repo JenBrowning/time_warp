462788f1ae2ad61ee63d7a4b68f2d29a726c26f6
Jennifer Browning <browningjen@yahoo.com>
2019-04-28 18:12:12 -0400

    starting project, added readme, set up app.py

---
+++ README.md
@@ -0,0 +1,6 @@
+Time Warp Extra Credit Assessment
+
+Create a working generate_key() function.
+
+Create a working validate_key() function.
+
---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+import hashlib
+import argparse
+
+
+
+def generate_key():
+
+
+
+def validate_key():
+
